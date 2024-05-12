1c87199bf54efc0d69d0ec04996ae93b02c930eb
GOLDEN-ZTU <23b_gai@liceum.ztu.edu.ua>
2024-05-12 12:34:38 +0300

    1

--- market.js
+++ market.js
@@ -1,24 +1,22 @@
-
 let itemsArray = [
-'Газонокосарка 43',
-'Електричний тример 110',
-'Електрична газонокосарка 32',
-'Акумуляторний оприскувач 12 N',
-]
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N',
+];
 
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
-    itemsArray.forEach((item,index) => {
+    itemsArray.forEach((item, index) => {
         itemsDiv.innerHTML +=
         `
         <div class="item">
-            <h2>Товар №${index}</h2>
+            <h2>Товар №${index + 1}</h2>
             <p>${item}</p>
         </div>
-        `
-    })
-} 
-else {
-    console.log('Блок товарів не знайдено')
+        `;
+    });
+} else {
+    console.log('Блок товарів не знайдено');
 }
