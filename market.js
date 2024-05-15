4fc224c459e9808f6a75bc9765978b98c4f1ca0b
GOLDEN-ZTU <158068598+GOLDEN-ZTU@users.noreply.github.com>
2024-05-15 12:31:34 +0300

    1

--- market.js
+++ market.js
@@ -1,28 +1,29 @@
 let itemsArray = [
-    'Газонокосарка 43',
-    'Електричний тример 110',
-    'Електрична газонокосарка 32',
-    'Акумуляторний оприскувач 12 ',
-    'Газонокосарка 43',
-    'Електричний тример 110',
-    'Електрична газонокосарка 30',
-    'Акумуляторний оприскувач 10 ',
-    'Газонокосарка 44',
-    'Електричний тример 111',
-    'Електрична газонокосарка 31',
-    'Акумуляторний оприскувач 13'
+{
+    firsName:"Віталій",
+    lastName:"Шатківський",
+    age: 43,
+    subject:"CS",
+    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+    url:"https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/",
+},
 ];
 
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
-    itemsArray.forEach((item, index) => {
-        let itemNumber = (index + 1).toString().padStart(2, ''); 
+    itemsArray
+    .forEach((item, index) => {
         itemsDiv.innerHTML +=
         `
         <div class="item">
             <h2>товар №${index+1} з ${itemsArray.length}</h2>
-            <p>${item}</p>
+            <p>${item.firsName} ${item.lastName}</p>
+            <p>Предмет:${item.subject}</p>
+            <p><img src=${item.photo}" class= "item-image"></p>
+            <p><a href="${item.url}" target="_blank" class="bonus price">Профіль</a></p>
+            <p><a href="mailto:${item.email}?subject=Питання&body=Доброго дня"target="_blank"
+            class="price">Написати</a></p>
         </div>
         `;
     });
